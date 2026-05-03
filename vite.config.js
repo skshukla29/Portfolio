5f63a28b488a0e38db0101464e1fbed8ce026b02
Shashikant Shukla <itzzshashikant29@gmail.com>
2026-05-03 21:22:01 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,31 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor';
+            }
+            if (id.includes('gsap')) {
+              return 'gsap';
+            }
+            if (id.includes('framer-motion')) {
+              return 'motion';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
